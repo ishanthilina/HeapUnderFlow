6f17a76ed1e87fa3d422575d1bda5d66e41f1978
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-12-25 22:15:17 +0530

    Added basic CRUD operations. http://www.yiiframework.com/doc/blog/1.1/en/prototype.scaffold

---
+++ protected/models/Question.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * This is the model class for table "{{question}}".
+ *
+ * The followings are the available columns in table '{{question}}':
+ * @property integer $id
+ * @property string $title
+ * @property string $content
+ * @property string $tags
+ * @property integer $resolved
+ * @property integer $score
+ * @property integer $create_time
+ * @property integer $update_time
+ * @property integer $author_id
+ *
+ * The followings are the available model relations:
+ * @property Answer[] $answers
+ * @property User $author
+ */
+class Question extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return '{{question}}';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('title, content, author_id', 'required'),
+			array('resolved, score, create_time, update_time, author_id', 'numerical', 'integerOnly'=>true),
+			array('title', 'length', 'max'=>128),
+			array('tags', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, title, content, tags, resolved, score, create_time, update_time, author_id', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'answers' => array(self::HAS_MANY, 'Answer', 'question_id'),
+			'author' => array(self::BELONGS_TO, 'User', 'author_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'title' => 'Title',
+			'content' => 'Content',
+			'tags' => 'Tags',
+			'resolved' => 'Resolved',
+			'score' => 'Score',
+			'create_time' => 'Create Time',
+			'update_time' => 'Update Time',
+			'author_id' => 'Author',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('title',$this->title,true);
+		$criteria->compare('content',$this->content,true);
+		$criteria->compare('tags',$this->tags,true);
+		$criteria->compare('resolved',$this->resolved);
+		$criteria->compare('score',$this->score);
+		$criteria->compare('create_time',$this->create_time);
+		$criteria->compare('update_time',$this->update_time);
+		$criteria->compare('author_id',$this->author_id);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Question the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
