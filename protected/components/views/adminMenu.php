4a23d483081e24bbb26584b6b17a86f58e9ee809
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2013-12-26 23:57:52 +0530

    Admin sidebar updated

--- protected/components/views/adminMenu.php
+++ protected/components/views/adminMenu.php
@@ -1,5 +1,7 @@
 <ul>
     <li><?php echo CHtml::link('Create New Question',array('question/create')); ?></li>
     <li><?php echo CHtml::link('Manage Users',array('user/admin')); ?></li>
+    <li><?php echo CHtml::link('Manage Questions',array('question/admin')); ?></li>
+    <li><?php echo CHtml::link('Manage Answers',array('answer/admin')); ?></li>
     <li><?php echo CHtml::link('Logout',array('site/logout')); ?></li>
 </ul>
